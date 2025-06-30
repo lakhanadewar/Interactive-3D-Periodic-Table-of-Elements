a78af3a3039a7bd0418945d1fd7180967553332e
Lakhan Adewar <lakhanadewar@gmail.com>
2025-06-30 17:24:48 +0000

    is is possible to show Model top view and zoomed in?

--- src/components/ElementModelViewer.tsx
+++ src/components/ElementModelViewer.tsx
@@ -33,7 +33,7 @@ export default function ElementModelViewer({ name, modelUrl }: ElementModelViewe
     
     // Camera
     const camera = new THREE.PerspectiveCamera(45, currentMount.clientWidth / currentMount.clientHeight, 0.1, 1000);
-    camera.position.set(0, 0, 8);
+    camera.position.set(0, 5, 0);
 
     // Renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
